7208911d16ccf0ff7a1b6440e5fa89561512b4b4
osteofelidae <azrieldelta@gmail.com>
2022-10-20 10:29:43 -0700

    Initial commit

---
+++ functionexpander.py
@@ -0,0 +1,16 @@
+inFileName = input("Input input file path... ")
+outFileName = input("Input output file path... ")
+
+inFile = open(inFileName, "r")
+outFile = open(outFileName, "w")
+
+
+
+
+
+
+
+
+
+inFile.close()
+outFile.close()
