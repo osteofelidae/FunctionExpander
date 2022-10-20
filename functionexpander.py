f5e622a4cf53871b390c7b440033977bab4e03ad
osteofelidae <azrieldelta@gmail.com>
2022-10-20 10:44:18 -0700

    findFunction draft

--- functionexpander.py
+++ functionexpander.py
@@ -1,11 +1,44 @@
+def findIndentLevel(strInput):
+    spaceCount = 0
+    checkIndex = 0
+    if strInput != "":
+        while strInput[checkIndex] == " ":
+            spaceCount += 1
+            checkIndex += 1
+    indentLevel = int(spaceCount / 4)
+    return indentLevel
+
+def removeIndent(strInput):
+    indentLevel = findIndentLevel(strInput)
+    if indentLevel != 0:
+        strOutput = strInput[indentLevel*5-1:]
+    else:
+        strOutput = strInput
+    return strOutput
+
+def findFunction(fileArrayInput, indexInput):
+    arrayOutput = []
+    cursorIndex = indexInput + 1
+    functionIndentLevel = findIndentLevel(fileArrayInput[indexInput])
+    while findIndentLevel(fileArrayInput[cursorIndex]) > functionIndentLevel:
+        arrayOutput.append(fileArrayInput[cursorIndex])
+        cursorIndex += 1
+    return arrayOutput
+    
+    
+    
+    
+
+
 inFileName = input("Input input file path... ")
 outFileName = input("Input output file path... ")
 
 inFile = open(inFileName, "r")
 outFile = open(outFileName, "w")
 
+inFileArray = inFile.readlines()
 
-
+print(findFunction(inFileArray, 0))
 
 
 
